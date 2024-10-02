92a938bcb288f7e933eb338f2d24c213d04d7be2
sarashaikhh <thewildcardnyc@gmail.com>
2024-10-02 15:51:20 -0400

    update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const video = document.getElementById('fullscreen-video');
 
+// Play or pause the video based on scroll position
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        video.play();
+    } else {
+        video.pause();
+    }
+});
